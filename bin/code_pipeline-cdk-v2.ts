d0bfa5470b136338fcdfbc91376ee9fdbfa7e091
Zaid748-collab <zaidaj319@gmail.com>
2026-06-20 15:39:10 +0100

    Final version

---
+++ bin/code_pipeline-cdk-v2.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CiCdAwsPipelineDemoStack } from '../lib/code_pipeline-cdk-v2-stack';
+
+const app = new cdk.App();
+new CiCdAwsPipelineDemoStack(app, 'CiCdAwsPipelineDemoStack', {
+  env: {
+    account: '001520131116',
+    region: 'eu-west-2',
+  }
+});
+
+app.synth();
