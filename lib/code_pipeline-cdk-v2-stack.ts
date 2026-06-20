4d7c3d7cfa1d0c9900be3397eadf089ec5005f73
Zaid748-collab <zaidaj319@gmail.com>
2026-06-20 15:58:34 +0100

    Added repo

--- lib/code_pipeline-cdk-v2-stack.ts
+++ lib/code_pipeline-cdk-v2-stack.ts
@@ -11,7 +11,7 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Zaid748-collab/CodePipeline-CDK-V2.git', 'main'), //Remember to change 
+        input: CodePipelineSource.gitHub('Zaid748-collab/CodePipeline-CDK-V2', 'main'), //Remember to change 
         commands: ['npm ci',
           'npm run build',
           'npx cdk synth']
